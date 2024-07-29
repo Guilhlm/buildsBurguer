43a312820ad6229edf2f7361c776f6cd634ac998
Guilherme Maia <141831938+Guilhlm@users.noreply.github.com>
2024-07-29 20:34:27 -0300

    classes criadas

---
+++ classe/Produto.php
@@ -0,0 +1,95 @@
+<?php
+class Produto
+{
+    public function CadastroProduto()
+    {
+        if (isset($_SESSION['nivel']) && !empty($_SESSION['nivel']) && isset($_POST) && !empty($_POST)) {
+
+            if (isset($_FILES['imagem']) && $_FILES['imagem']['error'] == 0) {
+
+                $titulo = $_POST['titulo'];
+                $preco = $_POST['preco'];
+                $imagem = $_FILES['imagem'];
+                $categoria = $_POST['categoria'];
+                $descricao = $_POST['descricao'];
+                $titulo = htmlspecialchars($titulo); // coverte em string
+                $preco = floatval($preco);
+                $categoria = htmlspecialchars($categoria);
+                $descricao = htmlspecialchars($descricao);
+
+                $nomeCaminho = './assets/img/produtos/' . round(microtime(true)) . '-' . basename($imagem['name']);
+                move_uploaded_file($imagem['tmp_name'], $nomeCaminho);
+
+
+                try {
+
+                    $conn = new PDO("mysql:host=62.72.62.1;dbname=u687609827_gui", "u687609827_gui", "Ou]Q||Jr^7H");
+                    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+                    $ScriptCadastro = $conn->prepare("INSERT INTO tb_produtos (titulo, imagem, preco, categoria, descricao) VALUES (:titulo, :imagem, :preco, :categoria, :descricao)");
+                    $ScriptCadastro->bindParam(':titulo', $titulo);
+                    $ScriptCadastro->bindParam(':imagem', $nomeCaminho);
+                    $ScriptCadastro->bindParam(':preco', $preco);
+                    $ScriptCadastro->bindParam(':categoria', $categoria);
+                    $ScriptCadastro->bindParam(':descricao', $descricao);
+                    $ScriptCadastro->execute();
+
+                    echo "<script>alert('produto cadastrado');</script>";
+                } catch (PDOException) {
+                    echo "<script>alert('seguinte deu uma coisa no treco');</script>";
+                }
+            } else {
+
+                echo "<script>alert('seguinte deu erro');</script>";
+            }
+        }
+    }
+
+    public function ListarProdutos()
+    {
+        $conn = new PDO("mysql:host=62.72.62.1;dbname=u687609827_gui", "u687609827_gui", "Ou]Q||Jr^7H");
+        $scriptListagemProduto = "SELECT * FROM tb_produtos";
+        $resultadoListagemProduto = $conn->query($scriptListagemProduto)->fetchAll();
+        return $resultadoListagemProduto;
+    }
+
+    public function Listar1Produto($id)
+    {
+        $conn = new PDO("mysql:host=62.72.62.1;dbname=u687609827_gui", "u687609827_gui", "Ou]Q||Jr^7H");
+        $scriptListar1Produto = "SELECT * FROM tb_produtos WHERE id = {$id}";
+        $resultado = $conn->query($scriptListar1Produto)->fetch();
+        return $resultado;
+    }
+
+    public function FiltroProduto()
+    {
+        if (empty($_POST['buscar'])) {
+
+            $buscar = "";
+        } else {
+
+            $buscar = $_POST['buscar'];
+        }
+
+        $conn = new PDO("mysql:host=62.72.62.1;dbname=u687609827_gui", "u687609827_gui", "Ou]Q||Jr^7H");
+        $consulta = $conn->prepare("SELECT * FROM tb_produtos WHERE titulo LIKE :buscar ORDER BY RAND()");
+
+        $buscaComCuringa = "%" . $buscar . "%";
+        $consulta->bindParam(':buscar', $buscaComCuringa);
+        $result = $consulta->execute();
+        $result = $consulta->fetchAll(PDO::FETCH_ASSOC);
+    }
+
+    public function DeletarProduto()
+    {
+        $conn = new PDO("mysql:host=62.72.62.1;dbname=u687609827_gui", "u687609827_gui", "Ou]Q||Jr^7H");
+        $scriptDeleteProduto = "DELETE FROM tb_produtos WHERE id = :id";
+
+        $preparo = $conn->prepare($scriptDeleteProduto);
+
+        $preparo->execute([
+
+            // ":id" => $id_delete
+        ]);
+    }
+}
---
+++ classe/Usuario.php
@@ -0,0 +1,152 @@
+<?php
+class Usuario
+{
+    public function LoginUser()
+    {
+        try {
+
+            if (isset($_POST['usuario']) && !empty($_POST['usuario'])) {
+
+                $user = $_POST['usuario'];
+                $password = $_POST['senha'];
+                $nome = $_POST['nome'];
+                $cpf = $_POST['cpf'];
+                $ano_nascimento = $_POST['ano_nascimento'];
+                $telefone = $_POST['telefone_1'];
+                $id = $_POST['id'];
+
+                $conn = new PDO("mysql:host=62.72.62.1;dbname=u687609827_gui", "u687609827_gui", "Ou]Q||Jr^7H");
+                $scriptLogin = "SELECT * FROM tb_usuarios INNER JOIN tb_pessoas ON tb_usuarios.id_pessoa = tb_pessoas.id WHERE usuario = '{$user}' AND senha = '{$password}'";
+                $resultadoLogin = $conn->query($scriptLogin)->fetch();
+
+                if (!empty($resultadoLogin)) {
+
+                    session_start();
+
+                    $_SESSION["usuario"] = $user;
+                    $nivel = $resultadoLogin['nivel'];
+                    $_SESSION["nivel"] = $nivel;
+
+                    
+                    $_SESSION['id'] = $resultadoLogin['id'];
+                    $_SESSION['nome'] = $resultadoLogin['nome'];
+                    $_SESSION['cpf'] = $resultadoLogin['cpf'];
+                    $_SESSION['telefone_1'] = $resultadoLogin['telefone_1'];
+                    $_SESSION['ano_nascimento'] = $resultadoLogin['ano_nascimento'];
+
+                    header('location: editarUsuario.php');
+                    sleep(1);
+                } else {
+                    $erro_login = "Usuário ou senha incorretos.";
+                }
+            }
+            header('location:index.php');
+        } catch (PDOException $erro) {
+            echo "Seguinte, deu erro no negocio do treco <br>" . $erro->getMessage();
+        }
+    }
+
+    public function CadastroUser()
+    {
+        try {
+
+            if (isset($_POST['nomeCadastro']) && !empty($_POST['nomeCadastro'])) {
+
+                $nome = $_POST['nomeCadastro'];
+                $usuario = $_POST['usuarioCadastro'];
+                $senha = $_POST['senhaCadastro'];
+                $Confirmar = $_POST['ConfirmarCadastro'];
+
+                $conn = new PDO("mysql:host=62.72.62.1;dbname=u687609827_gui", "u687609827_gui", "Ou]Q||Jr^7H");
+                $scriptCadastroPessoa = "INSERT INTO tb_pessoas (nome) VALUE ('{$nome}')";
+                $resultadoCadastroPessoa = $conn->prepare($scriptCadastroPessoa)->execute([]);
+                $pessoaID = $conn->lastInsertId();
+
+                $scriptCadastro = "INSERT INTO tb_usuarios (nome, usuario, senha, id_pessoa) VALUE ('{$nome}', '{$usuario}', '{$senha}', '{$pessoaID}')";
+                $resultadoCadastro = $conn->prepare($scriptCadastro)->execute([]);
+            }
+        } catch (PDOException $erro) {
+            echo "Seguinte, deu erro no negocio do treco <br>" . $erro->getMessage();
+        }
+    }
+
+    public function CadastroADM()
+    {
+        if (isset($_SESSION['nivel']) && !empty($_SESSION['nivel']) && isset($_POST) && !empty($_POST)) {
+
+            try {
+
+                $nome = $_POST['nome'];
+                $email = $_POST['email'];
+                $senha = $_POST['senha'];
+                $nivel = $_POST['nivel'];
+
+                $conn = new PDO("mysql:host=62.72.62.1;dbname=u687609827_gui", "u687609827_gui", "Ou]Q||Jr^7H");
+                $scriptCadastroPessoinha = "INSERT INTO tb_pessoas (nome) VALUE ('{$nome}')";
+                $resultadoCadastroADM = $conn->prepare($scriptCadastroPessoinha)->execute([]);
+                $pessoaID = $conn->lastInsertId();
+
+                $scriptCadastroADM = "INSERT INTO tb_usuarios (nome, usuario, senha, id_pessoa, nivel) VALUE ('{$nome}', '{$email}', '{$senha}', '{$pessoaID}', '{$nivel}')";
+                $resultadoCadastro = $conn->prepare($scriptCadastroADM)->execute([]);
+
+                echo "<script>alert('ADMIN cadastrado');</script>";
+            } catch (PDOException) {
+                echo "<script>alert('seguinte deu uma coisa no treco');</script>";
+            }
+        }
+    }
+
+    public function ListarUsuarios()
+    {
+        $conn = new PDO("mysql:host=62.72.62.1;dbname=u687609827_gui", "u687609827_gui", "Ou]Q||Jr^7H");
+        $scriptListarUsuario = "SELECT * FROM tb_usuarios INNER JOIN tb_pessoas ON tb_usuarios.id_pessoa = tb_pessoas.id";
+        $resultadoListagemUsuario = $conn->query($scriptListarUsuario)->fetchAll();
+        return $resultadoListagemUsuario;
+    }
+
+    public function AtualizarUsuario()
+    {
+
+        if ($_SERVER['REQUEST_METHOD'] === 'POST' && !empty($_POST)) {
+
+            try {
+                $id = $_POST['id'];
+                $nome = $_POST['nome'];
+                $telefone_1 = $_POST['telefone_1'];
+                $cpf = $_POST['cpf'];
+                $ano_nascimento = $_POST['ano_nascimento'];
+
+                $conn = new PDO("mysql:host=62.72.62.1;dbname=u687609827_gui", "u687609827_gui", "Ou]Q||Jr^7H");
+                $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+                $scriptUpdatePessoa = "UPDATE tb_pessoas SET nome = :nome, ano_nascimento = :ano_nascimento, cpf = :cpf, telefone_1 = :telefone_1 WHERE id = :id";
+                $variaveis = $conn->prepare($scriptUpdatePessoa);
+
+                $variaveis->execute([
+                    ':id' => $id,
+                    ':nome' => $nome,
+                    ':ano_nascimento' => $ano_nascimento,
+                    ':cpf' => $cpf,
+                    ':telefone_1' => $telefone_1
+                ]);
+
+                echo "<script>alert('Update realizado, por favor deslogue');</script>";
+            } catch (PDOException) {
+                echo "<script>alert('seguinte deu uma coisa no treco');</script>";
+            }
+        }
+    }
+
+    public function usuarioDelete()
+    {
+        $conn = new PDO("mysql:host=62.72.62.1;dbname=u687609827_gui", "u687609827_gui", "Ou]Q||Jr^7H");
+        $scriptDeleteUser = "DELETE FROM tb_usuarios WHERE id = :id";
+
+        $preparo = $conn->prepare($scriptDeleteUser);
+
+        $preparo->execute([
+
+            // ":id" => $id_delete
+        ]);
+    }
+}
